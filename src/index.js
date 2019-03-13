e5fea28567ccd8ef2abf78e0c43548534d85e932
Derek Lam <dc.lam@mail.utoronto.ca>
2019-03-13 11:28:10 -0400

    Lift metadata fetching logic to index

--- src/index.js
+++ src/index.js
@@ -10,40 +10,45 @@ const hash = document.location.hash.substring(1);
 const maybe_annotation = params.get('annotation');
 // const maybe_dataset = params.get('dataset');
 const maybe_dataset = 'EDMSE_pat_FR_1096_050.mat';
-if(maybe_annotation != null || maybe_dataset != null) {
-	const F_initial_range = (() => {
-		const maybe_start = params.get('start');
-		const maybe_range = params.get('range');
-		if(maybe_annotation != null) {
-			return d3.json(`annotation?id=${maybe_annotation}`).then(d => ({
-				dataset: d.dataset,
-				start: d.start,
-				range: d.range
-			}));
-		}
-		else if(maybe_start != null && maybe_range != null) {
-			return Q({
-				dataset: maybe_dataset,
-				start: parseFloat(start),
-				range: parseFloat(range)
-			});
-		}
-		else {
-			return d3.json(`dataset_meta?dataset=${maybe_dataset}`).then(d => ({
-				dataset: maybe_dataset,
-				start: 0,
-				end: d.point_count / d.Fs - EPS
-			}));
-			// TODO URGENT make this case work
-			// F_meta.then({ point_count, Fs } => D.resolve([maybe_dataset, [0, point_count / Fs]])); // beware of numerical inaccuracy!
-		}
-	})();
-	window.addEventListener('load', () => {
-		F_initial_range.then(initial =>
-			ReactDOM.render(<MainController {...initial} />, document.getElementById('main'))
-		);
-	});
-}
-else {
-	// TODO dataset selection screen
-}
+const F_initial = (() => {
+	if(maybe_annotation != null || maybe_dataset != null) {
+		return (() => {
+			const maybe_start = params.get('start');
+			const maybe_range = params.get('range');
+			if(maybe_annotation != null) {
+				return d3.json(`annotation?id=${maybe_annotation}`); // expect server to also fetch dataset metadata
+			}
+			else if(maybe_start != null && maybe_range != null) {
+				return d3.json(``Q({
+					dataset: maybe_dataset,
+					start: parseFloat(start),
+					// range: parseFloat(range)
+				});
+			}
+			else {
+				return d3.json(`dataset_meta?dataset=${maybe_dataset}`).then(d => Object.assign({}, d, {
+					dataset: maybe_dataset,
+					start: parseFloat(start) || 0,
+					// range: d.point_count / d.Fs - EPS
+				}));
+				// TODO URGENT make this case work
+				// F_meta.then({ point_count, Fs } => D.resolve([maybe_dataset, [0, point_count / Fs]])); // beware of numerical inaccuracy!
+			}
+		})();
+	}
+	else {
+		return Q.fcall(() => { throw new Exception(); });
+	}
+})();
+F_initial.then(meta => {
+	if(document.readyState === 'complete') {
+		ReactDOM.render(<MainController dataset_meta={meta} />, document.getElementById('main'));
+	}
+	else {
+		window.addEventListener('load', () => {
+			ReactDOM.render(<MainController dataset_meta={meta} />, document.getElementById('main'));
+		});
+	}
+}, e => {
+	// retry or show dataset selection screen
+})
