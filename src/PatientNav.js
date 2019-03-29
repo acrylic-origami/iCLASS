0da84b6eee955da2c0fb395c566013671a8320be
Sasha Rudolf 15 <sasha.rudolf@gmail.com>
2019-03-29 16:00:30 -0400

    Further styling for demo

--- src/PatientNav.js
+++ src/PatientNav.js
@@ -7,14 +7,40 @@ import { Route,
 export default class extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.patientTitleStyle = {
+			marginLeft: "30px",
+			marginTop: "30px"
+		}
+
+		this.listWrapStyle = {
+			marginTop: "20px",
+			marginBottom: "20px"
+		};
+
+		this.datasetWrapStyle = {
+			width: '960px',
+			position: 'relative',
+			height: '50px',
+			marginTop: '-1px',
+			marginLeft: '10px'
+		};
+
+		this.linkWrapStyle = {
+			marginLeft: '30px',
+			lineHeight: '40px',
+		};
 	}
 
 	render = () =>
 		<div>
-			<h1>iCLASS</h1>
-			<ul className="header">
+			<h1 style={this.patientTitleStyle}>iCLASS</h1>
+			<div style={{marginLeft: '30px'}}> Patients:</div>
+			<ul>
 				{this.props.patientAccounts.map((id) => 
-					<li key={"pat-" + id}><Link to={{pathname: "/" + id}}>{id}</Link></li>
+					<li style={this.linkWrapStyle} key={"pat-" + id}>
+						<Link to={{pathname: "/" + id}}>{id}</Link>
+					</li>
 				)}
 			</ul>
 			{(this.props.patientNotFound) ? <div>Patient not found.</div> : <div></div>}
